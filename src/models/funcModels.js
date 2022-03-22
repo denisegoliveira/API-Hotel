718cd13595f79478fba5032c754b5971625e3b2e
Denise Oliveira <goliveira.denise@gmail.com>
2022-03-21 21:23:15 -0300

    fix: ajustes no models

--- src/models/funcModels.js
+++ src/models/funcModels.js
@@ -8,27 +8,31 @@ const funcionarios = sequelize.define("funcionarios", {
     allowNull: false, //não habilita campo nulo
     validate: {
       notEmpty: {
-        msg: "Esse campo precisa ser preenchido",
-      },
-    },
+        msg: "O campo precisa ser preenchido somente com seu nome",
+      }
+    }
   },
   sobrenome: {
     type: DataTypes.STRING,
     allowNull: false,
     validate: {
       notEmpty: {
-        msg: "Esse campo precisa ser preenchido",
-      },
-    },
+        msg: "O campo precisa ser preenchido com seu sobrenome",
+      }
+    }
   },
   cpf: {
     type: DataTypes.STRING,
     allowNull: false,
     validate: {
       isAlphanumeric: {
-        msg: "Esse campo deve conter somente os números do CPF",
+        msg: "O campo precisa ser preenchido com os números do CPF",
       },
-    },
+      len: {
+        args: [11, 11],
+        msg: 'Escreva somente os números do seu CPF'
+      }
+    }
   },
   cargo: {
     type: DataTypes.STRING,
@@ -37,29 +41,29 @@ const funcionarios = sequelize.define("funcionarios", {
       isIn: {
         args: [["gerente", "recepcionista", "camareira", "supervisor"]],
         msg: "Escolha um cargo entre as opções disponiveis",
-      },
-    },
+      }
+    }
   },
   dataAdmissao: {
     type: DataTypes.DATEONLY,
     allowNull: false,
     validate: {
       isDate: true,
-    },
+    }
   },
+  //ajustar validação do salario
   salario: {
     type: DataTypes.FLOAT,
     allowNull: false,
     validate: {
       isAlphanumeric: {
-        msg: "Insira um valor",
+        msg: "Insira um valor sem pontos e sem vírgulas",
       },
       len: {
-        //incluir limitação de caracteres
         args: [4, 7],
-        msg: "O valor precisa ter entre 4 e 7 caracteres",
-      },
-    },
+        msg: "O valor precisa ser um núemro entre 4 e 7 digitos",
+      }
+    }
   },
   turno: {
     type: DataTypes.STRING,
@@ -69,9 +73,9 @@ const funcionarios = sequelize.define("funcionarios", {
         isIn: {
           args: [["matutino", "vespertino", "noturno"]],
           msg: "Escolha um turno entre matutino, vespertino e noturno",
-        },
-      },
-    },
+        }
+      }
+    }
   },
   email: {
     type: DataTypes.STRING,
@@ -79,18 +83,22 @@ const funcionarios = sequelize.define("funcionarios", {
     validate: {
       isEmail: {
         msg: "Preencha com um email válido",
-      },
-    },
+      }
+    }
   },
   telefone: {
     type: DataTypes.STRING,
     allowNull: false,
     validate: {
       isAlphanumeric: {
-        msg: "Escreva somente os números do telefone",
+        msg: "Preencha com os números de seu telefone",
       },
-    },
-  },
+      len: {
+        args: [10, 11],
+        msg: 'Preencha somente com os números do telefone, incluindo DDD'
+      }
+    }
+  }
 });
 
 
